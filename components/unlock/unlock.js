21060b1871e41f045eff3c9f43a4669afeba2f8c
antonnell <antonnell.crpto@gmail.com>
2021-05-10 17:53:07 +0200

    Added a connect wallet prompt on the project page. Don't like it, but it will do.
    Signed-off-by: antonnell <antonnell.crpto@gmail.com>

--- components/unlock/unlock.js
+++ components/unlock/unlock.js
@@ -1,73 +1,68 @@
-import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
-import { Typography, Button, CircularProgress } from "@material-ui/core";
-import CloseIcon from "@material-ui/icons/Close";
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { Typography, Button, CircularProgress } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 
-import { Web3ReactProvider, useWeb3React } from "@web3-react/core";
-import { Web3Provider } from "@ethersproject/providers";
+import { Web3ReactProvider, useWeb3React } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
 
-import {
-  ERROR,
-  CONNECTION_DISCONNECTED,
-  CONNECTION_CONNECTED,
-  CONFIGURE,
-} from "../../stores/constants";
+import { ERROR, CONNECTION_DISCONNECTED, CONNECTION_CONNECTED, CONFIGURE } from '../../stores/constants';
 
-import stores from "../../stores";
+import stores from '../../stores';
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
     flex: 1,
-    height: "auto",
-    display: "flex",
-    position: "relative"
+    height: 'auto',
+    display: 'flex',
+    position: 'relative',
   },
   contentContainer: {
-    margin: "auto",
-    textAlign: "center",
-    padding: "12px",
-    display: "flex",
-    flexWrap: "wrap"
+    margin: 'auto',
+    textAlign: 'center',
+    padding: '12px',
+    display: 'flex',
+    flexWrap: 'wrap',
   },
   cardContainer: {
-    marginTop: "60px",
-    minHeight: "260px",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-around",
-    alignItems: "center"
+    marginTop: '60px',
+    minHeight: '260px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
   unlockCard: {
-    padding: "24px"
+    padding: '24px',
   },
   buttonText: {
-    marginLeft: "12px",
-    fontWeight: "700"
+    marginLeft: '12px',
+    fontWeight: '700',
   },
   instruction: {
-    maxWidth: "400px",
-    marginBottom: "32px",
-    marginTop: "32px"
+    maxWidth: '400px',
+    marginBottom: '32px',
+    marginTop: '32px',
   },
   actionButton: {
-    padding: "12px",
-    backgroundColor: "white",
-    borderRadius: "3rem",
-    border: "1px solid #E1E1E1",
+    padding: '12px',
+    backgroundColor: 'white',
+    borderRadius: '3rem',
+    border: '1px solid #E1E1E1',
     fontWeight: 500,
-    [theme.breakpoints.up("md")]: {
-      padding: "15px"
-    }
+    [theme.breakpoints.up('md')]: {
+      padding: '15px',
+    },
   },
   connect: {
-    width: "100%"
+    width: '100%',
   },
   closeIcon: {
-    position: "absolute",
-    right: "-8px",
-    top: "-8px",
-    cursor: "pointer"
-  }
+    position: 'absolute',
+    right: '-8px',
+    top: '-8px',
+    cursor: 'pointer',
+  },
 });
 
 class Unlock extends Component {
@@ -76,7 +71,7 @@ class Unlock extends Component {
 
     this.state = {
       loading: false,
-      error: null
+      error: null,
     };
   }
 
@@ -87,25 +82,19 @@ class Unlock extends Component {
   }
 
   componentWillUnmount() {
-    stores.emitter.removeListener(
-      CONNECTION_CONNECTED,
-      this.connectionConnected
-    );
-    stores.emitter.removeListener(
-      CONNECTION_DISCONNECTED,
-      this.connectionDisconnected
-    );
+    stores.emitter.removeListener(CONNECTION_CONNECTED, this.connectionConnected);
+    stores.emitter.removeListener(CONNECTION_DISCONNECTED, this.connectionDisconnected);
     stores.emitter.removeListener(ERROR, this.error);
   }
 
-  error = err => {
+  error = (err) => {
     this.setState({ loading: false, error: err });
   };
 
   connectionConnected = () => {
     stores.dispatcher.dispatch({
       type: CONFIGURE,
-      content: { connected: true }
+      content: { connected: true },
     });
 
     if (this.props.closeModal != null) {
@@ -143,13 +132,8 @@ function getLibrary(provider) {
   return library;
 }
 
-function onConnectionClicked(
-  currentConnector,
-  name,
-  setActivatingConnector,
-  activate
-) {
-  const connectorsByName = stores.accountStore.getStore("connectorsByName");
+function onConnectionClicked(currentConnector, name, setActivatingConnector, activate) {
+  const connectorsByName = stores.accountStore.getStore('connectorsByName');
   setActivatingConnector(currentConnector);
   activate(connectorsByName[name]);
 }
@@ -167,21 +151,13 @@ function onDeactivateClicked(deactivate, connector) {
 
 function MyComponent(props) {
   const context = useWeb3React();
-  const localContext = stores.accountStore.getStore("web3context");
+  const localContext = stores.accountStore.getStore('web3context');
   var localConnector = null;
   if (localContext) {
     localConnector = localContext.connector;
   }
-  const {
-    connector,
-    library,
-    account,
-    activate,
-    deactivate,
-    active,
-    error
-  } = context;
-  var connectorsByName = stores.accountStore.getStore("connectorsByName");
+  const { connector, library, account, activate, deactivate, active, error } = context;
+  var connectorsByName = stores.accountStore.getStore('connectorsByName');
 
   const { closeModal } = props;
 
@@ -196,7 +172,7 @@ function MyComponent(props) {
     if (account && active && library) {
       stores.accountStore.setStore({
         account: { address: account },
-        web3context: context
+        web3context: context,
       });
       stores.emitter.emit(CONNECTION_CONNECTED);
     }
@@ -207,82 +183,76 @@ function MyComponent(props) {
   return (
     <div
       style={{
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: width > 576 ? "space-between" : "center",
-        alignItems: "center"
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: width > 576 ? 'space-between' : 'center',
+        alignItems: 'center',
       }}
     >
-      {Object.keys(connectorsByName).map(name => {
+      {Object.keys(connectorsByName).map((name) => {
         const currentConnector = connectorsByName[name];
         const activating = currentConnector === activatingConnector;
-        const connected =
-          currentConnector === connector || currentConnector === localConnector;
+        const connected = currentConnector === connector || currentConnector === localConnector;
         const disabled = !!activatingConnector || !!error;
 
         let url;
         let display = name;
-        let descriptor = "";
-        if (name === "MetaMask") {
-          url = "/connectors/icn-metamask.svg";
-          descriptor = "Connect to your MetaMask wallet";
-        } else if (name === "WalletConnect") {
-          url = "/connectors/walletConnectIcon.svg";
-          descriptor = "Scan with WalletConnect to connect";
-        } else if (name === "TrustWallet") {
-          url = "/connectors/trustWallet.png";
-          descriptor = "Connect to your TrustWallet";
-        } else if (name === "Portis") {
-          url = "/connectors/portisIcon.png";
-          descriptor = "Connect with your Portis account";
-        } else if (name === "Fortmatic") {
-          url = "/connectors/fortmaticIcon.png";
-          descriptor = "Connect with your Fortmatic account";
-        } else if (name === "Ledger") {
-          url = "/connectors/icn-ledger.svg";
-          descriptor = "Connect with your Ledger Device";
-        } else if (name === "Squarelink") {
-          url = "/connectors/squarelink.png";
-          descriptor = "Connect with your Squarelink account";
-        } else if (name === "Trezor") {
-          url = "/connectors/trezor.png";
-          descriptor = "Connect with your Trezor Device";
-        } else if (name === "Torus") {
-          url = "/connectors/torus.jpg";
-          descriptor = "Connect with your Torus account";
-        } else if (name === "Authereum") {
-          url = "/connectors/icn-aethereum.svg";
-          descriptor = "Connect with your Authereum account";
-        } else if (name === "WalletLink") {
-          display = "Coinbase Wallet";
-          url = "/connectors/coinbaseWalletIcon.svg";
-          descriptor = "Connect to your Coinbase wallet";
-        } else if (name === "Frame") {
-          return "";
+        let descriptor = '';
+        if (name === 'MetaMask') {
+          url = '/connectors/icn-metamask.svg';
+          descriptor = 'Connect to your MetaMask wallet';
+        } else if (name === 'WalletConnect') {
+          url = '/connectors/walletConnectIcon.svg';
+          descriptor = 'Scan with WalletConnect to connect';
+        } else if (name === 'TrustWallet') {
+          url = '/connectors/trustWallet.png';
+          descriptor = 'Connect to your TrustWallet';
+        } else if (name === 'Portis') {
+          url = '/connectors/portisIcon.png';
+          descriptor = 'Connect with your Portis account';
+        } else if (name === 'Fortmatic') {
+          url = '/connectors/fortmaticIcon.png';
+          descriptor = 'Connect with your Fortmatic account';
+        } else if (name === 'Ledger') {
+          url = '/connectors/icn-ledger.svg';
+          descriptor = 'Connect with your Ledger Device';
+        } else if (name === 'Squarelink') {
+          url = '/connectors/squarelink.png';
+          descriptor = 'Connect with your Squarelink account';
+        } else if (name === 'Trezor') {
+          url = '/connectors/trezor.png';
+          descriptor = 'Connect with your Trezor Device';
+        } else if (name === 'Torus') {
+          url = '/connectors/torus.jpg';
+          descriptor = 'Connect with your Torus account';
+        } else if (name === 'Authereum') {
+          url = '/connectors/icn-aethereum.svg';
+          descriptor = 'Connect with your Authereum account';
+        } else if (name === 'WalletLink') {
+          display = 'Coinbase Wallet';
+          url = '/connectors/coinbaseWalletIcon.svg';
+          descriptor = 'Connect to your Coinbase wallet';
+        } else if (name === 'Frame') {
+          return '';
         }
 
         return (
           <div
             key={name}
             style={{
-              padding: "0px",
-              display: "flex",
-              margin: width > 576 ? "12px 0px" : "0px"
+              padding: '0px',
+              display: 'flex',
+              margin: width > 576 ? '12px 0px' : '0px',
             }}
           >
             <Button
               style={{
-                width: width > 576 ? "350px" : "calc(100vw - 100px)",
-                height: "160px"
+                width: width > 576 ? '350px' : 'calc(100vw - 100px)',
+                height: '160px',
               }}
               variant="contained"
               onClick={() => {
-                onConnectionClicked(
-                  currentConnector,
-                  name,
-                  setActivatingConnector,
-                  activate
-                );
+                onConnectionClicked(currentConnector, name, setActivatingConnector, activate);
               }}
               disableElevation
               color="secondary"
@@ -290,35 +260,33 @@ function MyComponent(props) {
             >
               <div
                 style={{
-                  height: "130px",
-                  width: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "space-evenly"
+                  height: '130px',
+                  width: '100%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  justifyContent: 'space-evenly',
                 }}
               >
                 <img
                   style={{
-                    width: "40px",
-                    height: "40px"
+                    width: '40px',
+                    height: '40px',
                   }}
                   src={url}
                   alt=""
                 />
-                <Typography variant={"h2"}>{display}</Typography>
-                <Typography variant={"body2"}>{descriptor}</Typography>
-                {activating && (
-                  <CircularProgress size={15} style={{ marginRight: "10px" }} />
-                )}
+                <Typography variant={'h5'}>{display}</Typography>
+                <Typography variant={'body2'}>{descriptor}</Typography>
+                {activating && <CircularProgress size={15} style={{ marginRight: '10px' }} />}
                 {!activating && connected && (
                   <div
                     style={{
-                      background: "#4caf50",
-                      borderRadius: "10px",
-                      width: "10px",
-                      height: "10px",
-                      marginRight: "10px"
+                      background: '#4caf50',
+                      borderRadius: '10px',
+                      width: '10px',
+                      height: '10px',
+                      marginRight: '10px',
                     }}
                   ></div>
                 )}
--- pages/project/[project]/index.js
+++ pages/project/[project]/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
-import { Typography, Paper } from '@material-ui/core';
+import { Typography, Paper, Button } from '@material-ui/core';
 import Skeleton from '@material-ui/lab/Skeleton';
 
 import Layout from '../../../components/layout/layout.js';
@@ -18,7 +18,7 @@ import BigNumber from 'bignumber.js';
 import classes from './project.module.css';
 
 import stores from '../../../stores/index.js';
-import { ERROR, GET_PROJECT, PROJECT_RETURNED, GAUGES_CONFIGURED, GET_TOKEN_BALANCES, TOKEN_BALANCES_RETURNED } from '../../../stores/constants';
+import { ERROR, GET_PROJECT, PROJECT_RETURNED, GAUGES_CONFIGURED, GET_TOKEN_BALANCES, TOKEN_BALANCES_RETURNED, CONFIGURE_RETURNED, CONNECT_WALLET } from '../../../stores/constants';
 
 import { formatCurrency, formatAddress } from '../../../utils';
 
@@ -30,6 +30,7 @@ function Projects({ changeTheme }) {
 
   const [loading, setLoading] = useState(false);
   const [project, setProject] = useState(null);
+  const [account, setAccount] = useState(null);
 
   useEffect(function () {
     const projectReturned = (proj) => {
@@ -37,8 +38,15 @@ function Projects({ changeTheme }) {
       forceUpdate()
     }
 
+    const accountConfigured = () => {
+      setAccount(stores.accountStore.getStore('account'))
+    }
+
     stores.emitter.on(PROJECT_RETURNED, projectReturned);
     stores.emitter.on(TOKEN_BALANCES_RETURNED, projectReturned)
+    stores.emitter.on(CONFIGURE_RETURNED, accountConfigured)
+
+    setAccount(stores.accountStore.getStore('account'))
 
     if(router.query.project) {
       stores.dispatcher.dispatch({ type: GET_PROJECT, content: { id: router.query.project } });
@@ -48,6 +56,7 @@ function Projects({ changeTheme }) {
     return () => {
       stores.emitter.removeListener(PROJECT_RETURNED, projectReturned);
       stores.emitter.removeListener(TOKEN_BALANCES_RETURNED, projectReturned)
+      stores.emitter.removeListener(CONFIGURE_RETURNED, accountConfigured)
     };
   }, []);
 
@@ -71,24 +80,39 @@ function Projects({ changeTheme }) {
     router.push('/');
   };
 
+  const callConnect = () => {
+    stores.emitter.emit(CONNECT_WALLET)
+  }
+
   return (
     <Layout changeTheme={changeTheme} backClicked={backClicked}>
       <Header changeTheme={changeTheme} backClicked={backClicked} />
-      <div className={classes.projectContainer}>
-        <Balances project={project} />
-        <div className={classes.projectCardContainer}>
-          { (project && project.veTokenMetadata && BigNumber(project.veTokenMetadata.userLocked).gt(0)) &&
-            <div className={ classes.fakeGrid }>
-              <VeAssetModificationAmount project={project} />
-              <VeAssetModificationDuration project={project} />
-            </div>
-          }
-          { !(project && project.veTokenMetadata && BigNumber(project.veTokenMetadata.userLocked).gt(0)) &&
-            <VeAssetGeneration project={project} />
-          }
-          <GaugeVoting project={project} />
+
+      { account &&
+        <div className={classes.projectContainer}>
+          <Balances project={project} />
+          <div className={classes.projectCardContainer}>
+            { (project && project.veTokenMetadata && BigNumber(project.veTokenMetadata.userLocked).gt(0)) &&
+              <div className={ classes.fakeGrid }>
+                <VeAssetModificationAmount project={project} />
+                <VeAssetModificationDuration project={project} />
+              </div>
+            }
+            { !(project && project.veTokenMetadata && BigNumber(project.veTokenMetadata.userLocked).gt(0)) &&
+              <VeAssetGeneration project={project} />
+            }
+            <GaugeVoting project={project} />
+          </div>
         </div>
-      </div>
+      }
+      { !account &&
+        <div className={ classes.connectWallet}>
+          <Typography variant='h2'>Please connect your wallet to continue.</Typography>
+          <Button disableElevation className={classes.accountButton} variant="contained" color="primary" onClick={callConnect}>
+            <Typography variant="h5">Connect Wallet</Typography>
+          </Button>
+        </div>
+      }
       <Footer />
     </Layout>
   );
